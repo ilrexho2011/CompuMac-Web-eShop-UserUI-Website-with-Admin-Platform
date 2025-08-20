a0386b7ffdf7ec4c2cfeaaee2f06f9f26803f515
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2025-08-20 13:56:04 +0200

    Add files via upload

---
+++ aplikacioni/config/email.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['email_config'] = array();
+
+
+// 'mailtype' => 'html',
+// 'protocol' => 'SMTP',
+// 'smtp_crypto' => 'ssl',
+// 'smtp_host' => 'smtp.gmail.com',
+// 'smtp_port' => '587',
+// 'smtp_user' => 'ilreincpi@gmail.com',
+// 'smtp_pass' => 'ijymoduwdjmxatyz', // api pass 
+// 'charset' => 'utf-8',
+// 'newline' => '\r\n',
+
+// $config['protocol'] = 'smtp';
+// $config['smtp_host'] = 'smtp.gmail.com';
+// $config['smtp_port'] = 587; // 465 if using SSL
+// $config['smtp_user'] = 'ilreincpi@gmail.com';
+// $config['smtp_pass'] = 'ijymoduwdjmxatyz'; // NOT your Gmail password, use App Password
+// $config['smtp_crypto'] = 'tls'; // ssl for port 465
+// $config['mailtype'] = 'html';
+// $config['charset'] = 'utf-8';
+// $config['wordwrap'] = TRUE;
+// $config['newline'] = "\r\n"; // very important for SMTP
+// $config['crlf'] = "\r\n";    // very important for SMTP
+
+?>
